3012be0949863842c2fcc9c3d3d8a6ba8be1b051
Christian Laforte <christian.laforte@gmail.com>
2025-02-26 07:29:39 -0500

    support cifar10 and mnist

---
+++ train_cifar10.py
@@ -0,0 +1,54 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import Dataset
+
+class CIFAR10Dataset(Dataset):
+    def __init__(self, image_size=32):
+        self.transform = transforms.Compose([
+            transforms.Resize(image_size),
+            transforms.ToTensor(),
+        ])
+        
+        self.dataset = datasets.CIFAR10(
+            root='./data', 
+            train=True,
+            download=True, 
+            transform=self.transform
+        )
+    
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, idx):
+        img, _ = self.dataset[idx]  # Ignore labels
+        return img
+
+from rectified_flow_pytorch import RectifiedFlow, Unet, Trainer
+
+def main():
+    image_size = 32
+    cifar_dataset = CIFAR10Dataset(image_size=image_size)
+    
+    # Smaller model for CIFAR-10
+    model = Unet(
+        dim=32,
+        channels=3,  # RGB images
+        dim_mults=(1, 2, 4),  # Smaller multipliers for low-res images
+    )
+
+    rectified_flow = RectifiedFlow(model)
+
+    trainer = Trainer(
+        rectified_flow,
+        dataset=cifar_dataset,
+        batch_size=128,  # Can use larger batches for smaller images
+        num_train_steps=50_000,
+        results_folder='./results/cifar10',
+        checkpoints_folder='./checkpoints/cifar10',
+        accelerate_kwargs={'log_with':'comet_ml'}
+    )
+
+    trainer()
+
+if __name__ == "__main__":
+    main()
---
+++ train_mnist.py
@@ -0,0 +1,54 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import Dataset
+
+class MNISTDataset(Dataset):
+    def __init__(self, image_size=28):
+        self.transform = transforms.Compose([
+            transforms.Resize(image_size),
+            transforms.ToTensor(),
+        ])
+        
+        self.dataset = datasets.MNIST(
+            root='./data',
+            train=True,
+            download=True,
+            transform=self.transform
+        )
+    
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, idx):
+        img, _ = self.dataset[idx]  # Ignore labels
+        return img
+
+from rectified_flow_pytorch import RectifiedFlow, Unet, Trainer
+
+def main():
+    image_size = 28
+    mnist_dataset = MNISTDataset(image_size=image_size)
+    
+    # Simple model for MNIST
+    model = Unet(
+        dim=32,
+        channels=1,  # Grayscale images
+        dim_mults=(1, 2, 4),  # Smaller multipliers for low-res images
+    )
+
+    rectified_flow = RectifiedFlow(model)
+
+    trainer = Trainer(
+        rectified_flow,
+        dataset=mnist_dataset,
+        batch_size=512,  # Can use larger batches for smaller images
+        num_train_steps=30_000,
+        results_folder='./results/mnist',
+        checkpoints_folder='./checkpoints/mnist',
+        accelerate_kwargs={'log_with':'comet_ml'}
+    )
+
+    trainer()
+
+if __name__ == "__main__":
+    main()
